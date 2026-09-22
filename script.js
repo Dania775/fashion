a0b148d72a8b2e67ee833f6a3178ba59de00b056
Dania <daniaadham102@gmail.com>
2026-09-23 00:25:18 +0200

    rettet productlist

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-const endpoint = "              ";
+const endpoint = "https://kea-alt-del.dk/t7/api/products?limit=10";
 
-const produktListe = document.querySelector(".productlist");
+const productList = document.querySelector(".productlist");
 
 fetch(endpoint)
-  .then((res) => res.json())
-  .then(visData);
+  .then((response) => response.json())
+  .then((data) => {
+    console.table(data);
 
-function visData(json) {
-  console.table(json);
-
-  json.forEach((produkt) => {
-    produktListe.innerHTML += `
-      <article class="card">
-        <img
-          src="https://kea-alt-del.dk/t7/images/webp/640/${produkt.id}.webp"
-          alt="${produkt.productdisplayname}"
-        >
-
-        <h2>${produkt.brandname}</h2>
-        <h3>${produkt.productdisplayname}</h3>
-        <p>${produkt.price} kr.</p>
-      </article>
-    `;
+    data.forEach((produkt) => {
+      productList.innerHTML += `
+        <article class="card">
+          <img src="https://kea-alt-del.dk/t7/images/webp/640/${produkt.id}.webp" alt="${produkt.productdisplayname}">
+          <h2>${produkt.brandname}</h2>
+          <h3>${produkt.productdisplayname}</h3>
+          <p>${produkt.price} kr.</p>
+        </article>
+      `;
+    });
   });
-}
